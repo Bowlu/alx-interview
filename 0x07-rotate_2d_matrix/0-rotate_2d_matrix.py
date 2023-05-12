7dff5495b8e2219c50949c071d8ee806093a8a68
Bowlu <awoyemiboluwatife@gmail.com>
2023-05-12 10:01:23 +0100

    adjusted code

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,20 +3,12 @@
 """Rotating a two-dimensional matrix 90degress clockwise"""
 
 def rotate_2d_matrix(matrix):
-  rotateLeft = 0,
-  rotateRight = 0,
-  len(matrix) - 1
-  
-  while rotateLeft < rotateRight:
-    for i in range(rotateRight -rotateLeft):
-      top = rotateLeft,
-      bottom = rotateRight
-      topRotateLeft = matrix[top][rotateLeft + i]
-      
-      matrix[top][rotateLeft + i] = matrix[bottom -i][rotateLeft]
-      matrix[bottom - i][rotateLeft]= matrix[bottom][rotateRight - i]
-      matrix[bottom][rotateRight - i] = matrix[top + i][rotateRight]
-      matrix[top + i][rotateRight] = topRotateLeft
+ matrixLength = len(matrix)
+ 
+ for i in range(matrixLength):
+   for l in range(i, matrixLength):
+     matrix[i][l], matrix[l][i] = matrix[l][i], matrix[i][l]
+     
+ for i in range(matrixLength):
+   matrix[i] = matrix[i][::-1]
     
-    rotateLeft = rotateLeft + 1
-    rotateRight = rotateRight - 1
