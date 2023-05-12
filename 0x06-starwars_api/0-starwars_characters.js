3ed833135f616d0eb9474a80b25bdcf06df850df
Bowlu <awoyemiboluwatife@gmail.com>
2023-05-12 10:13:30 +0100

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,24 @@
 
 const request = require('request');
 
-const movieID = process.argv[2];
-const movieEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieID;
-
-
-request(movieEndPoint, (res, error, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const movie = JSON.parse(body);
-    const characterUrl = movie.characters;
-
-    for (const movieCharacter of characterUrl) {
-      request(movieCharacter, (res, error, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const movieCharacter = JSON.parse(body);
-          console.log(movieCharacter.name);
-        }
-      });
+const movieURL = 'https://swapi-api.hbtn.io/api/films/';
+if (process.argv.length > 2) {
+  request(`${movieURL}/films/${process.argv[2]}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
     }
-  }
-});
+    const characterURL = JSON.parse(body).characters;
+    const characterName = characterURL.map(url => new Promise((resolve, reject) => {
+      request(url, (promiseErr, emp, characterBody) => {
+        if (promiseErr) {
+          reject(promiseErr);
+        }
+        resolve(JSON.parse(characterBody).name);
+      });
+    }));
+
+    Promise.all(characterName)
+      .then(cNames => console.log(cNames.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
