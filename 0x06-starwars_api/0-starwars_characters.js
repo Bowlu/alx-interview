8a340c0c928d189c7b66003e757f8b659e431798
Bowlu <awoyemiboluwatife@gmail.com>
2023-05-12 11:25:05 +0100

    adjusted error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,33 @@
 
 const request = require('request');
 
-const movieURL = 'https://swapi-api.hbtn.io/api/films/';
-if (process.argv.length > 2) {
-  request(`${movieURL}/films/${process.argv[2]}/`, (err, res, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const characterURL = JSON.parse(body).characters;
-    const characterName = characterURL.map(url => new Promise((resolve, reject) => {
-      request(url, (promiseErr, emp, characterBody) => {
-        if (promiseErr) {
-          reject(promiseErr);
-        }
-        resolve(JSON.parse(characterBody).name);
-      });
-    }));
+const movieURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-    Promise.all(characterName)
-      .then(cNames => console.log(cNames.join('\n')))
-      .catch(allErr => console.log(allErr));
+request(movieURL, asyn function (error, res, body) {
+  const array = [];
+
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    for (let r = 0; r < movie.characters.lenth; r++) {
+      array.push(movieCharacter(movie.characters[r]));
+    }
+  }
+
+  let cast = await Promise.all(array);
+
+  cast = cast.map((actor) => JSON.parse(actor).name);
+  cast.forEach((actor) => console.log(actor));
+});
+
+function movieCharacter (eachCharacter) {
+  return new Promise((resolve, reject) => {
+    request(eachCharacter, function (error, res, body) {
+      if (error) {
+        reject(error);
+      }
+      resolve(body);
+    });
   });
 }
