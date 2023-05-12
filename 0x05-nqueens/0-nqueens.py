2b797c90a79f0dac16dc93e7caecc3c0ebe769be
Bowlu <awoyemiboluwatife@gmail.com>
2023-05-12 10:49:28 +0100

    N queens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,61 +1,91 @@
 #!/usr/bin/python3
 
 """Program that solves the N queens problem"""
-
-
 import sys
 
 
-"""Function prints board"""
-def printBoard(board, num):
-  array = []
-  for i in range(num):
-    for j in range(num):
-      if j == board[i]:
-        array.append([i, j])
-  print(array)
-  
-  
-"""Function determines the safe 
-position for the queen"""
-def validPosition(board, i, j, l):
-    return board[i] in (j, j - i + l, i - l + j)
-  
-  
-def positions(board, row, num):
-  if row == num:
-    printBoard(board, num)
+if__name__ == '__main__':
+  code = sys.argv
+  if len(code) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+    
+  try:
+    n = int(code[1])
+  except ValueError:
+    print('N must be a number')
+    exit(1)
+    
+  if n < 4:
+    print('N must be at least 4')
+    exit(1)
+    
+  column = 0
+  row = 0
+  stop = False
+  result = []
+  queenPosition = []
+    
+  while row < n:
+    reverse = False
+    while column < n:
+        
+      safe = True
+      for post in queenPosition:
+        p = post[1]
+        if (p == column or p + (row-post[0]) == column or p - (row-post[0]) == column):
+          safe = False
+          break
+        
+        if not safe:
+          if column == n - 1:
+            reverse = True
+            break
+          column = column + 1
+          continue
+        
+        
+        posts = [row, column]
+        queenPosition.append(posts)
+        
+        if row == n -1:
+          result.append(queenPosition[:])
+          for post in queenPosition:
+            if post[1] < n - 1:
+              row = post[0]
+              column = post[1]
+          for r in range(n - row):
+            queenPosition.pop()
+          if row == n - 1 and column == n - 1:
+            queenPosition = []
+            stop = True
+            
+          row = row - 1
+          column = column + 1
+          
+        else:
+          column = 0
+        break
       
-  else: 
-    for j in range(num):
-      allowed = True
-      for i in range(row):
-        if validPosition(board, i, j, row):
-          allowed = False
-      if allowed:
-        board[row] = j
-        positions(board, row + 1, num)
-        
-        
-def createBoard(length):
-  return [0 * length for i in range(length)]
-
-
-if len(sys.arg) != 2:
-  print("Usage: nqueens N")
-  sys.exit(1)
-  
-try:
-  num = int(sys.argv[1])
-except BaseException:
-  print("N must be a number")
-  sys.exit(1)
-  
-if (num < 4):
-  print("N must be at least 4")
-  sys.exti(1)
-  
-
-row = 0
-board = createBoard(int(num))
-positions(board, row, int(num))
+      if stop:
+        break
+      
+      if reverse:
+        row = row - 1
+        while row >= 0:
+          column = queenPosition[row][1] + 1
+          del queenPosition[row]
+          if column < n:
+            break
+          row = row - 1
+        if row < 0:
+          break
+        continue
+      row = row + 1
+      
+    for idx, val in enumerate(result):
+      if idx == len(result) - 1:
+        print(val)
+      else:
+        print(val)
+    
